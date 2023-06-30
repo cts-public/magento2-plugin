0f9a73a57edcb44e760ad341184a6666eb1ea7b9
siarheicts <138124423+siarheicts@users.noreply.github.com>
2023-06-30 17:21:49 +0200

    Update Callback.php

--- Controller/Payment/Callback.php
+++ Controller/Payment/Callback.php
@@ -105,15 +105,6 @@ class Callback implements HttpPostActionInterface, CsrfAwareActionInterface
         }
 
         $payment = $order->getPayment();
-//        $token = $this->request->getParam(self::TOKEN_KEY) ?? '';
-//
-//        if (!$token || $token !== $payment->getAdditionalInformation(CryPayPayment::CRYPAY_ORDER_TOKEN_KEY)) {
-//            return $this->response->setStatusHeader(
-//                Response::STATUS_CODE_422,
-//                AbstractMessage::VERSION_11,
-//                self::UNPROCESSABLE_CONTENT_PHRASE
-//            );
-//        }
 
         if (!$this->crypayPayment->validateCryPayCallback($order, $requestBody, $signature)) {
             return $this->response->setStatusHeader(
@@ -126,98 +117,6 @@ class Callback implements HttpPostActionInterface, CsrfAwareActionInterface
         $this->eventManager->dispatch('crypay_merchant_callback_send', ['order' => $order]);
 
         return $this->response->setStatusHeader(Response::STATUS_CODE_200, AbstractMessage::VERSION_11);
-
-        //$this->request = Tools::file_get_contents('php://input');
-        //            $this->logInfo('CryPay reportPayload: ' . $this->request);
-        //            $headers = $this->get_ds_headers();
-        //            if (!array_key_exists("XSignature", $headers)) {
-        //                $error_message = 'CryPay X-SIGNATURE: not found';
-        //                $this->logError($error_message);
-        //                throw new Exception($error_message, 400);
-        //            }
-        //
-        //            $signature = $headers["XSignature"];
-        //
-        //            $this->requestData = json_decode($this->request, true);
-        //            if (false === $this->checkIfRequestIsValid()) {
-        //                $error_message = 'CryPay Request: not valid request data';
-        //                $this->logError($error_message);
-        //                throw new Exception($error_message, 400);
-        //            }
-        //
-        //            if ($this->requestData['type'] !== 'PAYMENT') {
-        //                $error_message = 'CryPay Request: not valid request type';
-        //                $this->logError($error_message);
-        //                throw new Exception($error_message, 400);
-        //            }
-        //
-        //            $cart_id = (int)$this->requestData['variableSymbol'];
-        //            $order_id = Order::getOrderByCartId($cart_id);
-        //            $order = new Order($order_id);
-        //            $currency = new Currency($order->id_currency);
-        //
-        //
-        //            if (!$cart_id) {
-        //                $error_message = 'Shop order #' . $this->requestData['variableSymbol'] . ' does not exists';
-        //                $this->logError($error_message, $cart_id);
-        //                throw new Exception($error_message, 400);
-        //            }
-        //
-        //            if ($currency->iso_code != $this->requestData['currency']) {
-        //                $error_message = 'CryPay Currency: ' . $this->requestData['currency'] . ' is not valid';
-        //                $this->logError($error_message, $cart_id);
-        //
-        //                throw new Exception($error_message, 400);
-        //            }
-        //
-        //            $apiKey = Configuration::get('CRYPAY_API_KEY');
-        //            $environment = (Configuration::get('CRYPAY_TEST')) == 1;
-        //            $client = new \CryPay\Client($apiKey, $environment);
-        //
-        //            $token = $client->generateSignature($this->request, Configuration::get('CRYPAY_API_SECRET'));
-        //
-        //            if (empty($signature) || strcmp($signature, $token) !== 0) {
-        //                $error_message = 'CryPay X-SIGNATURE: ' . $signature . ' is not valid. valid X-SIGNATURE ' . $token;
-        //                $this->logError($error_message, $cart_id);
-        //                throw new Exception($error_message, 400);
-        //            }
-        //
-        //            switch ($this->requestData['state']) {
-        //                case 'SUCCESS':
-        //                    if (((float)$order->getOrdersTotalPaid()) == ((float)$this->requestData['amount'])) {
-        //                        $order_status = 'PS_OS_PAYMENT';
-        //                        break;
-        //                    } else {
-        //                        $order_status = 'CRYPAY_INVALID';
-        //                        $this->logError('PS Orders Total does not match with Crypay Price Amount', $cart_id);
-        //                    }
-        //                    break;
-        //                case 'WAITING_FOR_PAYMENT':
-        //                    $order_status = 'CRYPAY_PENDING';
-        //                    break;
-        //                case 'WAITING_FOR_CONFIRMATION':
-        //                    $order_status = 'CRYPAY_CONFIRMING';
-        //                    break;
-        //                case 'EXPIRED':
-        //                    $order_status = 'CRYPAY_EXPIRED';
-        //                    break;
-        //                default:
-        //                    $order_status = false;
-        //            }
-        //
-        //            if ($order_status && Configuration::get($order_status) != $order->current_state && $order->current_state != Configuration::get('PS_OS_PAYMENT')) {
-        //                $history = new OrderHistory();
-        //                $history->id_order = $order->id;
-        //                $history->changeIdOrderState((int)Configuration::get($order_status), $order->id);
-        //                $history->addWithemail(true, array(
-        //                    'order_name' => $cart_id,
-        //                ));
-        //
-        //                $this->response('OK');
-        //
-        //            } else {
-        //                $this->response('Order Status ' . $this->requestData['state'] . ' not implemented');
-        //            }
     }
 
     /**
